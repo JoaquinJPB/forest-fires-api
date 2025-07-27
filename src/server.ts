c4ed470aed6b3dd463cdfba287ea5f535e628956
Joaquin Pastore <jjpb2000@gmail.com>
2025-07-27 15:11:48 +0100

    Add YAML dependency and implement OpenAPI documentation for the Forest Fires API

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,32 @@
 import Fastify from "fastify";
 import allRoutes from "./routes";
+import swagger from "@fastify/swagger";
+import swaggerUI from "@fastify/swagger-ui";
+import fs from "fs";
+import yaml from "yaml";
 
 const fastify = Fastify({
   logger: true,
 });
 
+// Load your openapi.yaml
+const openapiDoc = yaml.parse(fs.readFileSync("./openapi.yaml", "utf8"));
+
 const start = async () => {
   try {
+    await fastify.register(swagger, {
+      mode: "static",
+      specification: {
+        document: openapiDoc,
+      },
+    });
+    await fastify.register(swaggerUI, {
+      routePrefix: "/docs",
+      uiConfig: {
+        docExpansion: "full",
+        deepLinking: false,
+      },
+    });
     fastify.register(allRoutes);
     await fastify.listen({ port: 3000, host: "0.0.0.0" });
   } catch (err) {
